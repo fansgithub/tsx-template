b5e6d641415cbbf3c821078495d06e9b180d7aaa
fanxing <18404985916@163.com>
2019-10-29 16:06:48 +0800

    chore: add comments to the .prettierrc.js file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,19 @@
 module.exports = {
+    //单行最大长度
     printWidth: 120,
+    //tab键的缩进
     tabWidth: 4,
+    //使用单引号而非双引号
     singleQuote: true,
+    //末尾分号
     semi: true,
+    //添加es5中被支持的尾逗号
     trailingComma: 'es5',
+    //在对象字面量声明所使用的的花括号后（{）和前（}）输出空格
     bracketSpacing: true,
+    //在多行JSX元素最后一行的末尾添加 > 而使 > 单独一行（不适用于自闭和元素）
     jsxBracketSameLine: true,
+    //为单行箭头函数的参数添加圆括号
     arrowParens: 'always',
-    parser: 'typescript',
+    parser: 'typescript'
 };
