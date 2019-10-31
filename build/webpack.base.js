421ac9ce4e166e0e39e970a4a8638b52d92deb12
fanxing <18404985916@163.com>
2019-10-31 17:21:57 +0800

    feat: change to single-page packaging

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -18,52 +18,9 @@ const gitRevisionCallback = (str, err) => {
   }
 };
 
-const setMPA = () => {
-  const entry = {};
-  const htmlWebpackPlugins = [];
-  const entryFiles = glob.sync(path.join(projectRoot, "./src/*/index.tsx"));
-
-  Object.keys(entryFiles).map(index => {
-    const entryFile = entryFiles[index];
-    const match = entryFile.match(/src\/(.*)\/index\.tsx/);
-    const pageName = match && match[1];
-
-    entry[pageName] = entryFile;
-    return htmlWebpackPlugins.push(
-      new HtmlWebpackPlugin({
-        inlineSource: ".css$",
-        template: path.join(projectRoot, `./src/${pageName}/index.html`),
-        filename: `${pageName}.html`,
-        favicon: `./src/${pageName}/assets/images/favicon.ico`,
-        chunks: [pageName],
-        inject: true,
-        banner: {
-          branch: gitRevision("branch", gitRevisionCallback),
-          tag: gitRevision("tag", gitRevisionCallback),
-          date: new Date().toLocaleString()
-        },
-        minify: {
-          html5: true,
-          collapseWhitespace: true,
-          preserveLineBreaks: false,
-          minifyCSS: true,
-          minifyJS: true,
-          removeComments: false
-        }
-      })
-    );
-  });
-
-  return {
-    entry,
-    htmlWebpackPlugins
-  };
-};
-
-const { entry, htmlWebpackPlugins } = setMPA();
 
 module.exports = {
-  entry: entry,
+  entry: './src/index.tsx',
   output: {
     path: path.join(projectRoot, "dist"),
     filename: "[name]_[chunkhash:8].js"
@@ -146,8 +103,29 @@ module.exports = {
     new webpack.DllReferencePlugin({
       manifest: path.resolve(__dirname, './dll/react.manifest.json')
     }),
-  ].concat([...htmlWebpackPlugins, new AddAssetHtmlPlugin({
-    filepath: path.resolve(__dirname, "./dll/**/*.js")
-  }),]),
+    new HtmlWebpackPlugin({
+      inlineSource: ".css$",
+      template: './src/index.html',
+      filename: 'index.html',
+      favicon: './src/assets/images/favicon.ico',
+      inject: true,
+      banner: {
+        branch: gitRevision("branch", gitRevisionCallback),
+        tag: gitRevision("tag", gitRevisionCallback),
+        date: new Date().toLocaleString()
+      },
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: false
+      }
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, "./dll/**/*.js")
+    })
+  ],
   stats: "errors-only"
 };
